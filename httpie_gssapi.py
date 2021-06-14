9595966a3bbeee356d84ce687d3f4d2ac5123c93
Martin Prpič <mprpic@redhat.com>
2021-06-14 16:12:12 -0400

    Bump version to 1.0.2

--- httpie_gssapi.py
+++ httpie_gssapi.py
@@ -13,7 +13,7 @@ except ImportError:
 from httpie.plugins import AuthPlugin
 from requests_gssapi import HTTPSPNEGOAuth, OPTIONAL, REQUIRED, DISABLED
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "Martin Prpic"
 __licence__ = "MIT"
 
